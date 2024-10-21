89ff7858452a36c9ba45949ecb2edb58a066e385
Harry Askham <harryaskham@gmail.com>
2024-10-21 17:18:41 +0000

    flake

--- flake.nix
+++ flake.nix
@@ -1,6 +1,17 @@
 {
   description = "owt";
 
+  nixConfig = {
+    extra-substituters = [
+      "https://ghc.cachix.org"
+      "https://nix-community.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "ghc.cachix.org-1:a751hwq9ydeP3Nr6h84iA9zSjxg9Z3uznqi4YBGjsiw="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
+
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
@@ -9,20 +20,19 @@
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs { inherit system; config = { allowBroken = true; }; };
         packageName = "owt";
-        hsPkgsPrev = pkgs.haskellPackages;
-        hsPkgs = hsPkgsPrev;
-        # hsPkgs = with pkgs; hsPkgsPrev.override {
-        #   overrides = final: prev: {
-        #     req-conduit = haskell.lib.dontCheck prev.req-conduit;
-        #   };
-        # };
-      in with pkgs; rec {
-        packages.${packageName} =
-          hsPkgs.callCabal2nixWithOptions "${packageName}" ./. "--no-check" {  };
+        pkgs = import nixpkgs { inherit system; config = { allowBroken = true; }; };
+        hsPkgs = pkgs.haskell.packages.ghc981.override {
+          overrides = final: prev: {
+            base64 = prev.base64_1_0;
+          };
+        };
+      in rec {
+        packages.${packageName} = hsPkgs.developPackage {
+          name = packageName;
+          root = ./.;
+        };
         packages.default = packages.${packageName};
-        devShells.default = mkShell {};
       }
     );
 }
